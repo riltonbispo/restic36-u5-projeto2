20cd1486877b482ebdacbcd5bef232de244835da
Rilton Bispo <riltondev@gmail.com>
2024-09-22 15:20:27 -0300

    refactor: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LoginFormComponent} from './components/login-form/login-form.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto2';
-}
+export class AppComponent {}
