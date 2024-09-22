eeb5a69a5ef07f79ab2bea6fbb277723316b61da
Rilton Bispo <riltondev@gmail.com>
2024-09-22 15:19:14 -0300

    feat: create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component'
+import { LoginFormComponent } from './components/login-form/login-form.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LoginFormComponent},
+  {path: 'feed', component: FeedComponent}
+];
